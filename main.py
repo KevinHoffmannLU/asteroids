b1224055d2b0022fd208584ed51e0745fc8f0cdf
KevinHoffmannLU <contact@hoffmannkevin.com>
2024-11-25 17:45:56 +0100

    creaing a player instance and drawing it on the screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -10,11 +11,16 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(0x000000)
+        
+        player.draw(screen)
+        
         pygame.display.flip()
         temp_dt = clock.tick(60)
         dt = temp_dt / 1000
